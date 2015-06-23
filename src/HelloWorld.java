67369dbb472905b99fb0d4c18018d48a87ee9e4c
unknown <jmander@PC-CZC451084M.netbuilder.private>
2015-06-23 17:02:39 +0100

    Added the SwingAppGUI from the lectures, it needs to be altered to produce a table of the products and their information

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,12 +8,14 @@ public class HelloWorld {
 			System.out.println("Hello World");
 			db = new DatabaseConnection();
 			db.accessDB();
-			db.createEntry();
+			//db.createEntry();
 			//db.readDB();
 			//db.updateDB();
 			//db.deleteRow();
 			db.readDB();
 			db.closeDB();
+			SwingAppGUI sD = new SwingAppGUI();
+			sD.showEvent();
 	}
 
 }
